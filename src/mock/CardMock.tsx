d345a25cf0374f4ef5484945117681a177cfbc35
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-17 16:09:06 -0300

    fix: diversificação de dados para teste de busca

--- src/mock/CardMock.tsx
+++ src/mock/CardMock.tsx
@@ -2,43 +2,43 @@ export const CardMock = [
   {
     product: "Lava louça",
     price: '4000',
-    category: 'casa',
-    date: '17/11/2023'
+    category: 'teste',
+    date: '03/11/2003'
   },
   {
     product: "Fogão",
     price: '2000',
-    category: 'casa',
-    date: '17/11/2023'
+    category: 'home',
+    date: '17/11/2073'
   },
   {
     product: "Panela",
     price: '109,99',
-    category: 'casa',
-    date: '17/11/2023'
+    category: 'casaa',
+    date: '19/11/1994'
   },
   {
     product: "Chuveiro",
     price: '300',
     category: 'casa',
-    date: '17/11/2023'
+    date: '15/11/2023'
   },
   {
     product: "TV",
     price: '4000',
-    category: 'casa',
-    date: '17/11/2023'
+    category: 'casa1',
+    date: '07/11/2023'
   },
   {
     product: "Sapateria",
     price: '1000',
-    category: 'casa',
-    date: '17/11/2023'
+    category: 'apartamento',
+    date: '01/11/2029'
   },
   {
     product: "Geladeira",
     price: '6000',
-    category: 'casa',
-    date: '17/11/2023'
+    category: 'edicula',
+    date: '01/11/2023'
   },
 ];
