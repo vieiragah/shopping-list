3109c76109c0e81642ed35c3237bee6fd2ba9f89
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-17 15:44:01 -0300

    feat: mock para consumo no card

---
+++ src/mock/CardMock.tsx
@@ -0,0 +1,44 @@
+export const CardMock = [
+  {
+    product: "Lava louça",
+    price: '4000',
+    category: 'casa',
+    date: '17/11/2023'
+  },
+  {
+    product: "Fogão",
+    price: '2000',
+    category: 'casa',
+    date: '17/11/2023'
+  },
+  {
+    product: "Panela",
+    price: '109,99',
+    category: 'casa',
+    date: '17/11/2023'
+  },
+  {
+    product: "Chuveiro",
+    price: '300',
+    category: 'casa',
+    date: '17/11/2023'
+  },
+  {
+    product: "TV",
+    price: '4000',
+    category: 'casa',
+    date: '17/11/2023'
+  },
+  {
+    product: "Sapateria",
+    price: '1000',
+    category: 'casa',
+    date: '17/11/2023'
+  },
+  {
+    product: "Geladeira",
+    price: '6000',
+    category: 'casa',
+    date: '17/11/2023'
+  },
+];
