f454bd6507ee87bf2cb70808a9c094bb8902a558
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-17 19:59:01 -0300

    feat: adição de radio button

---
+++ src/components/radio/Radio.tsx
@@ -0,0 +1,72 @@
+import {
+  Box,
+  HStack,
+  useRadioGroup,
+  useRadio,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import { ArrowUpIcon, ArrowDownIcon } from "@chakra-ui/icons";
+
+interface RadioCardProps {
+  children: React.ReactNode;
+  icon?: React.ReactNode;
+}
+
+function RadioCard({ children, icon, ...props }: RadioCardProps) {
+  const { getInputProps, getRadioProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getRadioProps();
+
+  return (
+    <Box as="label">
+      <Input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderRadius="md"
+        boxShadow="md"
+        _checked={{
+          bg: "#000000",
+          color: "white",
+        }}
+        px={5}
+        py={3}
+        display="inline-block"
+      >
+        <HStack spacing={2}>
+          {icon && <Box>{icon}</Box>}
+          <Text>{children}</Text>
+        </HStack>
+      </Box>
+    </Box>
+  );
+}
+
+export const Radio = () => {
+  const options = [
+    { label: "Entrada", icon: <ArrowUpIcon color="green" /> },
+    { label: "Saída", icon: <ArrowDownIcon color="red" /> },
+  ];
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: "value",
+    defaultValue: "entrada",
+  });
+
+  const group = getRootProps();
+
+  return (
+    <HStack {...group} justifyContent="center" margin="20px 0 0 0">
+      {options.map((option) => {
+        const radio = getRadioProps({ value: option.label });
+        return (
+          <RadioCard key={option.label} {...radio} icon={option.icon}>
+            {option.label}
+          </RadioCard>
+        );
+      })}
+    </HStack>
+  );
+};
