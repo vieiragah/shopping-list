e1ce46c3151373bfe64104c93ee7a82c5b93a77d
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-17 13:03:23 -0300

    feat : Inicialização do projeto e instalação de dependências

---
+++ src/group/Group.tsx
@@ -0,0 +1,8 @@
+export const Group = () => {
+  return (
+    <div>
+      <p>Criação do grpo</p>
+      <p>Adicionar pessoas</p>
+    </div>
+  );
+};
---
+++ src/list/List.tsx
@@ -0,0 +1,10 @@
+const List = () => {
+  return (
+    <div>
+      <p>input para adicionar item e valor</p>
+      <p>Tabela organizando item e valor</p>
+    </div>
+  );
+};
+
+export default List;
