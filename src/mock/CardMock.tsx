9f90b711f836d12dc8553ef1b6dca5cef9239d29
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-18 17:43:03 -0300

    fix: alterações de dados mock

--- src/mock/CardMock.tsx
+++ src/mock/CardMock.tsx
@@ -1,44 +1,58 @@
 export const CardMock = [
   {
     product: "Lava louça",
-    price: '4000',
+    price: 4000,
     category: 'teste',
-    date: '03/11/2003'
+    date: '03/11/2003',
+    entry: true,
+    exit: false,
   },
   {
     product: "Fogão",
-    price: '2000',
+    price: 2000,
     category: 'home',
-    date: '17/11/2073'
+    date: '17/11/2073',
+    entry: false,
+    exit: true,
   },
   {
     product: "Panela",
-    price: '109,99',
+    price: 109.99,
     category: 'casaa',
-    date: '19/11/1994'
+    date: '19/11/1994',
+    entry: true,
+    exit: false,
   },
   {
     product: "Chuveiro",
-    price: '300',
+    price: 300,
     category: 'casa',
-    date: '15/11/2023'
+    date: '15/11/2023',
+    entry: false,
+    exit: true,
   },
   {
     product: "TV",
-    price: '4000',
+    price: 4000,
     category: 'casa1',
-    date: '07/11/2023'
+    date: '07/11/2023',
+    entry: true,
+    exit: false,
   },
   {
     product: "Sapateria",
-    price: '1000',
+    price: 1000,
     category: 'apartamento',
-    date: '01/11/2029'
-  },
+    date: '01/11/2029',
+    entry: false,
+    exit: true,
+  }, 
   {
     product: "Geladeira",
-    price: '6000',
+    price: 6000,
     category: 'edicula',
-    date: '01/11/2023'
+    date: '01/11/2023',
+    entry: true,
+    exit: false,
   },
 ];
--- src/mock/CarouselMock.tsx
+++ src/mock/CarouselMock.tsx
@@ -1,22 +1,26 @@
-import { ArrowUpIcon, ArrowDownIcon, ArrowUpDownIcon } from '@chakra-ui/icons'
+import { ArrowUpIcon, ArrowDownIcon, ArrowUpDownIcon } from "@chakra-ui/icons";
 
 export const CarouselMock = [
   {
     title: "Entradas",
-    value: "17.400,00",
+    value: 17400,
     description: "Última entrada em 13 de abril",
-    icon: <ArrowUpIcon color='green'/>
+    icon: <ArrowUpIcon color="green" />,
+    bg: "#323238",
   },
   {
     title: "Saídas",
-    value: "1.400,00",
+    value: 1400,
     description: "Última entrada em 13 de abril",
-    icon: <ArrowDownIcon color='red'/>
+    icon: <ArrowDownIcon color="red" />,
+    bg: "#323238",
   },
   {
     title: "Total",
-    value: "16.000,00",
+    value: 16000,
     description: "Última entrada em 13 de abril",
-    icon: <ArrowUpDownIcon color='lightBlue'/>
+    icon: <ArrowUpDownIcon color="lightBlue" />,
+    bg: "#015F43",
   },
 ];
+CarouselMock[2].bg = CarouselMock[2].value < 0 ? "#6a121a" : "#015F43";
