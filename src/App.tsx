18c9b7e6066b5181b4678646cdf49bed07c86ab4
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-18 17:40:48 -0300

    fix: add cor de fundo com divisória

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Box } from '@chakra-ui/react'
 import { CardComponent, Carousel, Header } from "./components";
  function App() {
   return (
-    <>
+    <Box bgGradient="linear(to bottom, #121214 160px, #202024 160px)">
       <Header />
       <Carousel />
       <CardComponent/>
-    </>
+    </Box>
   );
 }
 
